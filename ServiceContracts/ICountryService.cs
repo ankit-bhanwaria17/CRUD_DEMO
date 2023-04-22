f59e145ee79c8131b9032beade8480461229a725
Ankit Bhanwaria <ankit.bhanwaria17@gmail.com>
2023-04-22 16:59:34 +0530

    Added Country Service Test

---
+++ ServiceContracts/ICountryService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    public interface ICountryService
+    {
+        public CountryAddResponse AddCountry(CountryAddRequest? Request);
+    }
+}
---
+++ xUnitTesting/CountryServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+using System;
+using System.Collections.Generic;
+
+
+namespace xUnitTesting
+{
+    public class CountryServiceTest
+    {
+        public readonly ICountryService _countryService;
+        public CountryServiceTest()
+        {
+            _countryService = new CountryService(); 
+        }
+
+        [Fact]
+        public void AddCountry_ArgumentNull()
+        {
+            CountryAddRequest request = null;
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                _countryService.AddCountry(request);
+            });
+        }
+
+        [Fact]
+        public void AddCountry_ArgumentInvalid()
+        {
+            CountryAddRequest request = new CountryAddRequest() { CountryName = null };
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _countryService.AddCountry(request);
+            });
+        }
+
+        [Fact]
+        public void AddCountry_DuplicateCountry()
+        {
+            CountryAddRequest request1 = new CountryAddRequest() { CountryName = "India" };
+            CountryAddRequest request2 = new CountryAddRequest() { CountryName = "India" };
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _countryService.AddCountry(request1);
+                _countryService.AddCountry(request2);
+            });
+        }
+
+        [Fact]
+        public void AddCountry_ProperCountryDetails()
+        {
+            CountryAddRequest request = new CountryAddRequest() { CountryName = "India" };
+
+            var response = _countryService.AddCountry(request);
+
+            Assert.True(response.CountryId != Guid.Empty);
+            
+        }
+    }
+}
